270e374785fcb66b0193b45407e97f45fd073e14
tulakann <tul@salweensolutions.com>
2018-07-17 10:21:56 +0700

    fix example views

--- example/App.js
+++ example/App.js
@@ -17,7 +17,7 @@ export default class MarqueeTextSample extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-
+        <View style={{ marginVertical: 20 }}>
           <MarqueeText
             style={{ fontSize: 24 }}
             duration={3000}
@@ -29,6 +29,7 @@ export default class MarqueeTextSample extends React.Component {
             Lorem Ipsum is simply dummy text of the printing and typesetting industry and typesetting industry.
           </MarqueeText>
         </View>
+
         <View style={{ marginVertical: 20 }}>
           <Text>Marquee On Start</Text>
           <MarqueeText
@@ -48,6 +49,7 @@ export default class MarqueeTextSample extends React.Component {
             />
           </View>
         </View>
+
         <View style={{ marginVertical: 20 }}>
           <Text>Marquee Text With Controls</Text>
           <MarqueeText ref={c => (this.marqueeTextRef0 = c)}>
@@ -58,6 +60,7 @@ export default class MarqueeTextSample extends React.Component {
             <Button title="Stop" onPress={() => this.marqueeTextRef0.stopAnimation()} />
           </View>
         </View>
+
         <View style={{ marginVertical: 20 }}>
           <Text>Marquee Text With Controls</Text>
           <MarqueeText style={{ fontSize: 20 }} ref={c => (this.marqueeTextRef = c)}>
@@ -68,6 +71,7 @@ export default class MarqueeTextSample extends React.Component {
             <Button title="Stop" onPress={() => this.marqueeTextRef.stopAnimation()} />
           </View>
         </View>
+
         <View>
           <Text>Fixed Text Width</Text>
           <MarqueeText style={{ fontSize: 16, width: 150 }} ref={c => (this.marqueeTextRef2 = c)}>
